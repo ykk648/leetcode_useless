1a2863f332ac10648647abf2b573580204888185
luanjintai <tylerluan@163.com>
2023-07-06 16:50:34 +0800

    链表旋转复用

--- linked_list.py
+++ linked_list.py
@@ -8,6 +8,95 @@ class ListNode:
         self.next = next
 
 
+def reverse_partial(start, end):
+    # 区域翻转(start-end之间的部分) 返回start-end区间中的头尾节点 inplace
+    cur, pre = start.next, start
+    dummy = cur
+    while cur != end:
+        temp = cur.next
+        cur.next = pre
+        pre = cur
+        cur = temp
+    start.next = pre
+    dummy.next = cur
+    return pre, dummy
+
+
+def reverse_partial_count(start, count):
+    # 从start处翻转count次， count = start-end+1与reverse_partial一致，返回start下一个节点
+    cur, pre = start.next, start
+    dummy = cur
+    while count > 0:
+        temp = cur.next
+        cur.next = pre
+        pre = cur
+        cur = temp
+        count -= 1
+    start.next = pre
+    dummy.next = cur
+    return pre, dummy
+
+
+class LinkReverse:
+    # 206 反转链表
+    def reverse(self, head):
+        cur, pre = head, None
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
+
+    # 反转链表 复用写法
+    def reverse_2(self, head):
+        if not head:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        res, _ = reverse_partial(dummy, None)
+        return res
+
+    # 92 反转链表II
+    def reverse_partial_from_head(self, head, left, right):
+        if not head:
+            return head
+            # 防止head被改变
+        dummy = ListNode(0)
+        dummy.next = head
+        # 开始遍历
+        start = dummy
+        end = head
+        count = 0
+        while end:
+            count += 1
+            if count < left:
+                start = start.next
+            else:
+                _, _ = reverse_partial_count(start, right - left + 1)
+                break
+        return dummy.next
+
+    # 25 K个一组翻转列表 复用写法
+    def reverse_k_group(self, head, k):
+        if not head or k == 1:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+
+        start = dummy
+        end = head
+        count = 0
+        while end:
+            count += 1
+            if count % k == 0:
+                _, start = reverse_partial(start, end.next)
+                end = start.next
+            else:
+                end = end.next
+        return dummy.next
+
+
 class Solution:
 
     # 141 环形链表
@@ -38,88 +127,6 @@ class Solution:
             fast, slow = fast.next, slow.next
         return fast
 
-    # 206 反转链表
-    def reverse(self, head):
-        cur, pre = head, None
-        while cur:
-            temp = cur.next
-            cur.next = pre
-            pre = cur
-            cur = temp
-        return pre
-
-    # 92 反转链表II
-    def reverse_partial_from_head(self, head, left, right):
-        if not head:
-            return head
-            # 防止head被改变
-        dummy = ListNode(0)
-        dummy.next = head
-        # 开始遍历
-        start = dummy
-        end = head
-        count = 0
-        while end:
-            count += 1
-            if count < left:
-                start = start.next
-            if count < right + 1:
-                end = end.next
-            print(count, left, right)
-            if count == right + 1 or not end:
-                start = self.reverse_partial(start, end)
-                end = start.next
-                break
-        return dummy.next
-
-    def reverse_partial_count(self, start, count):
-        # 从start处翻转count次
-        # 区域翻转(start-end之间的部分)
-        cur, pre = start.next, start
-        dummy = cur
-        while count > 0:
-            temp = cur.next
-            cur.next = pre
-            pre = cur
-            cur = temp
-            count -= 1
-        start.next = pre
-        dummy.next = cur
-        return pre
-
-    # 25 K个一组翻转列表
-    def reverse_partial(self, start, end):
-        # 区域翻转(start-end之间的部分)
-        cur, pre = start.next, start
-        dummy = cur
-        while cur != end:
-            temp = cur.next
-            cur.next = pre
-            pre = cur
-            cur = temp
-        start.next = pre
-        dummy.next = cur
-        return pre
-
-    def reverse_k_group(self, head, k):
-        if not head or k < 2:
-            return head
-        # 防止head被改变
-        dummy = ListNode(0)
-        dummy.next = head
-        # 开始遍历
-        start = dummy
-        end = head
-        count = 0
-        while end:
-            count += 1
-            if count % k == 0:
-                start = self.reverse_partial(start, end.next)
-                end = start.next
-            else:
-                end = end.next
-        return dummy.next
-
     # 21 合并两个有序链表 （递归）
     def merge_list_node(self, head1, head2):
         if not head1:
@@ -132,3 +139,25 @@ class Solution:
         else:
             head2.next = self.merge_list_node(head1, head2.next)
             return head2
+
+    # 328 奇偶链表
+    def odd_even_list(self, head):
+        if not head:
+            return head
+        odd = head
+        even = dummy = head.next
+        while odd.next and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = dummy
+        return head
+
+    # 160 相交链表 (拼接链表
+    def cross_list(self, head1, head2):
+        p1, p2 = head1, head2
+        while p1 != p2:
+            p1 = p1.next if p1 else head2
+            p2 = p2.next if p2 else head1
+        return p1
